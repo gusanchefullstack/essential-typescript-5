b6e3c69856e4d7c582fc71345f928c69779cbd40
Gustavo Sanchez Galarza <gustavosanchezgalarza@gmail.com>
2026-05-16 23:39:09 -0700

    Add todos folder to test examples

---
+++ todo/dist/todoCollection.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TodoCollection = void 0;
+const todoItem_1 = require("./todoItem");
+class TodoCollection {
+    userName;
+    nextId = 1;
+    itemMap = new Map();
+    constructor(userName, todoItems = []) {
+        this.userName = userName;
+        todoItems.forEach(item => this.itemMap.set(item.id, item));
+    }
+    addTodo(task) {
+        while (this.getTodoById(this.nextId)) {
+            this.nextId++;
+        }
+        this.itemMap.set(this.nextId, new todoItem_1.TodoItem(this.nextId, task));
+        return this.nextId;
+    }
+    getTodoById(id) {
+        return this.itemMap.get(id);
+    }
+    markComplete(id, complete) {
+        const todoItem = this.getTodoById(id);
+        if (todoItem) {
+            todoItem.complete = complete;
+        }
+    }
+}
+exports.TodoCollection = TodoCollection;
